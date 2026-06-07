8c8dcfbb099630ef7bbc4e9be5c47b80614d82f6
Andrey Ruch <andreyruch1@gmail.com>
2026-06-07 17:10:01 +0300

    refactor: implement logic for findFactorial, reverseString, and swap functions

--- src/app.js
+++ src/app.js
@@ -4,25 +4,33 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
+const findFactorial = function (num) {
+    if (num === 0 || num === 1) return 1;
 
-}
+    return num * findFactorial(num - 1);
+};
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
+function reverseString(str) {
+    if (str.length <= 1) return str;
 
+    return reverseString(str.slice(1)) + str[0];
 }
 
 //Exercise 3
-const arr1 = [1, 2, 3]
-const arr2 = []
+const arr1 = [1, 2, 3];
+const arr2 = [];
 
-const swap = function() {
-  //Your code here
+function swap(arr1, arr2) {
+    if (arr1.length === 0) return;
 
+    arr2.push(arr1.shift());
+    swap(arr1, arr2);
 }
 
+swap(arr1, arr2);
+console.log(arr1); //[]
+console.log(arr2); //[1, 2, 3]
+
 /* DO NOT REMOVE THE EXPORTS BELOW */
-module.exports = { findFactorial, reverseString, swap }
+module.exports = { findFactorial, reverseString, swap };
